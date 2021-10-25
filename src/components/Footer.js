c2d9ba5c0e6019d8f2e56e6537bce1f536668c4e
dlynnea <danimatton@gmail.com>
2021-10-24 22:10:48 -0600

    more clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,6 @@ const Footer = () => {
                     <p>Hotel Engine 2021</p>
                 </div>
             </section>
-            {/* <div className="container">
-                <div className="row">
-                    <div className="social">
-                        <a href='https://www.linkedin.com/in/danielle-matton/' target='_blank' rel='noopener noreferrer'>
-                            <i className='fab fa-linkedin' />
-                        </a>
-                        <a href='https://twitter.com/dlynneaa' target='_blank' rel='noopener noreferrer'>
-                            <i className='fab fa-twitter' />
-                        </a>
-                        <a href='https://github.com/dlynnea' target='_blank' rel='noopener noreferrer'>
-                            <i className='fab fa-github' />
-                        </a>
-                        <a href='https://medium.com/@danimatton' target='_blank' rel='noopener noreferrer'>
-                            <i className="fab fa-medium-m"></i>
-                        </a>
-                    </div>
-                </div>
-            </div> */}
         </footer>
     )
 }
